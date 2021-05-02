3babc6b2cf532def4eeb8313b7faa750cc8316f8
Jakub Przystasz <jakub.przystasz@gmail.com>
2021-05-02 16:26:30 +0200

    refactor add_layer

--- headers/lib/net.h
+++ headers/lib/net.h
@@ -15,7 +15,13 @@ class Net
     data_row cost;
     uint32_t layers_count;
 
+    void __add_layer(uint32_t neurons, double learning_rate, double momentum, size_t prev_weights);
+
 public:
+    double learning_rate;
+    double momentum;
+    uint32_t prev_weights;
+
     std::vector<Layer> layers;
 
     data_set input;
@@ -29,12 +35,12 @@ public:
         auto input_row = this->input[row_num];
         auto target_row = this->target[row_num];
         double cost_sum = 0;
-        
+
         //Feed net with data
         this->feed(input_row);
         //Calculate cost for each output neuron
         auto output_row = this->output[this->layers_count - 1];
-        
+
         for (uint32_t i{0}; i < this->output[layers_count - 1].size(); i++)
         {
             this->cost[i] = target_row[i] - output_row[i];
@@ -59,14 +65,16 @@ public:
     /**
      * Add layer to net
      */
-    void add_layer(uint32_t neurons, double learning_rate = 0.1, double momentum = 0.1, size_t prev_weights = 1);
+    void add_layer(uint32_t neurons, double learning_rate, double momentum, size_t prev_weights);
+
+    void add_layer(uint32_t neurons);
 
     /**
      *  Constructor of neural network
      *  @arg input - input data set
      *  @arg target - target data set
      */
-    Net(data_set &input, data_set &target);
+    Net(data_set &input, data_set &target, double learning_rate = 0.1, double momentum = 0.1, size_t prev_weights = 1);
 
     ~Net();
 };
--- src/lib/net.cpp
+++ src/lib/net.cpp
@@ -2,6 +2,16 @@
 
 void Net::add_layer(uint32_t neurons, double learning_rate, double momentum, size_t prev_weights)
 {
+    this->__add_layer(neurons, learning_rate, momentum, prev_weights);
+}
+
+void Net::add_layer(uint32_t neurons)
+{
+    this->__add_layer(neurons, this->learning_rate, this->momentum, this->prev_weights);
+}
+
+void Net::__add_layer(uint32_t neurons, double learning_rate, double momentum, size_t prev_weights)
+{
     if (this->layers.size() < 1)
         this->layers.push_back(Layer(neurons, neurons, prev_weights, learning_rate, momentum));
     else
@@ -16,14 +26,15 @@ void Net::add_layer(uint32_t neurons, double learning_rate, double momentum, siz
     this->layers_count += 1;
 }
 
-
-Net::Net(data_set &input, data_set &target)
+Net::Net(data_set &input, data_set &target, double learning_rate, double momentum, size_t prev_weights)
 {
     this->input = input;
     this->target = target;
     this->cost = data_row(target[0].size(), 0);
     this->layers_count = 0;
+    this->learning_rate = learning_rate;
+    this->momentum = momentum;
+    this->prev_weights = prev_weights;
 }
-
 
 Net::~Net() {}
